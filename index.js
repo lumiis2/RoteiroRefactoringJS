bca5936f2b2f238374c7c08c3494d802452b1388
Luisa Carvalhaes <luisalcarvalhaes@gmail.com>
2025-11-06 20:01:29 -0300

    Commit 1 - Extração de Função

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ function gerarFaturaStr (fatura, pecas) {
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
   
-    for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
+    // função extraída
+    function calcularTotalApresentacao(apre, peca) {
       let total = 0;
-  
       switch (peca.tipo) {
       case "tragedia":
         total = 40000;
@@ -29,6 +28,12 @@ function gerarFaturaStr (fatura, pecas) {
       default:
           throw new Error(`Peça desconhecia: ${peca.tipo}`);
       }
+      return total;
+    }
+  
+    for (let apre of fatura.apresentacoes) {
+      const peca = pecas[apre.id];
+      let total = calcularTotalApresentacao(apre, peca);
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
